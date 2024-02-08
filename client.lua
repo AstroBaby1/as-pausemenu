cfd812d842da04c9531bc2fc223a4e59623fefc5
SalahEddine <77403585+AstroBaby1@users.noreply.github.com>
2024-02-08 15:05:09 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,35 +1,42 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local Player = QBCore.Functions.GetPlayerData() 
 local open = false
+local loaded = false
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    loaded = true
+end)
+
 Citizen.CreateThread(function()
     while true do
         Wait(1)
         SetPauseMenuActive(false)
-        if (IsControlJustPressed(0, 200)) and not open and not IsPauseMenuActive() then
-            TransitionToBlurred(1000)
-            SetNuiFocus(true, true)
-            open = true
-
-            -- Call the callback to get playersOnline data
-            QBCore.Functions.TriggerCallback('astro:getplayers', function(playersOnline, policeCount, players)
-                print("heLLO")
-                local characterData = {
-                    characterName = Player.charinfo.firstname..' '..Player.charinfo.lastname,
-                    job = Player.job.name,
-                    cashAmount = Player.money['cash'],
-                    bankAmount = Player.money['bank'],
-                    playersOnline = playersOnline,
-                    policeAvailable = true 
-                }
-                SendNUIMessage({
-                    type = "characterData",
-                    data = characterData
-                })
-                SendNUIMessage({
-                    action = "show"
-                })
-            end)
-        end
+	if loaded then
+	        if (IsControlJustPressed(0, 200)) and not open and not IsPauseMenuActive() then
+	            TransitionToBlurred(1000)
+	            SetNuiFocus(true, true)
+	            open = true
+	
+	            -- Call the callback to get playersOnline data
+	            QBCore.Functions.TriggerCallback('astro:getplayers', function(playersOnline, policeCount, players)
+	                print("heLLO")
+	                local characterData = {
+	                    characterName = Player.charinfo.firstname..' '..Player.charinfo.lastname,
+	                    job = Player.job.name,
+	                    cashAmount = Player.money['cash'],
+	                    bankAmount = Player.money['bank'],
+	                    playersOnline = playersOnline,
+	                    policeAvailable = true 
+	                }
+	                SendNUIMessage({
+	                    type = "characterData",
+	                    data = characterData
+	                })
+	                SendNUIMessage({
+	                    action = "show"
+	                })
+	            end)
+	        end
+	end
     end
 end)
 
